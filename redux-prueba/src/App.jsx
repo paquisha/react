936c0a49c713141a094f30f58cc71d1ae869c88f
Andres Grijalva <paquishaxd@gmail.com>
2020-10-10 20:46:42 -0500

    cambios en componentes apra listados

--- redux-prueba/src/App.jsx
+++ redux-prueba/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Pokemones from './components/Pokemones';
+import {Provider} from 'react-redux'
+import generateStore from './redux/store'
 
 function App() {
+
+  const store = generateStore()
+
   return (
-    <div>
-      <h1>Redux</h1>
-    </div>
+    <Provider store={store}>
+      <Pokemones />
+    </Provider>
   );
 }
 
 export default App;
+ 
--- redux-prueba/src/redux/store.js
+++ redux-prueba/src/redux/store.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers,compose, applyMiddleware} from 'redux'
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 
 import pokeReducer from './pokeDucks'
