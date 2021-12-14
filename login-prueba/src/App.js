9a911108f4811c158d17fdd0d3e22ad4d6f16f0d
aekmot <paquishaxd@gmail.com>
2021-12-13 22:59:52 -0500

    prueba de login finalizada

--- login-prueba/src/App.js
+++ login-prueba/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //como se ve normalmente
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  //usando destructuring
+  const handlePasswordChange = ({ target: { value }}) => {
+    setPassword(value);
+  }
+
+  //formulario
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    alert(`Usuario: ${email}, Password: ${password}`);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit}>
+        <h2>iniciar sesion</h2>
+        <label>
+          Correo
+          <input type="email" value={email} onChange={handleEmailChange}/>
+        </label>
+        <label>
+          password
+          <input type="password" value={password} onChange={handlePasswordChange}/>
+        </label>
+        <button type='submit'>
+          Enter
+        </button>
+      </form>
     </div>
   );
 }
