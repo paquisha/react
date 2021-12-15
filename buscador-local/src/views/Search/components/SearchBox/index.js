164699464e72e6e6003d547eb60d00eaecc6a3f8
aekmot <paquishaxd@gmail.com>
2021-12-14 23:12:42 -0500

    Se finalizo ejercicio de react esta genial

--- buscador-local/src/views/Search/components/SearchBox/index.js
+++ buscador-local/src/views/Search/components/SearchBox/index.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 
-export default function SearchBox({ onSearch, onClose}) {
+export default function SearchBox({ onSearch, onClose, isSearching}) {
   const [searchText, setSearchText] = useState("");
+  
+  const handleSearchClick = () => {
+    setSearchText("");
+    onClose();
+  }
+
   return (
     <div className="search-box">
       <h2 className="search-box-titulo">Buscador de personal</h2>
@@ -14,8 +20,8 @@ export default function SearchBox({ onSearch, onClose}) {
             onChange={({ target: { value } }) => setSearchText(value)}
           />
         </label>
-        <button onClick={onSearch}>Buscar</button>
-        <button onClick={onClose}>Cerrar</button>
+        <button onClick={() => onSearch(searchText)} disabled={!searchText.length}>Buscar</button>
+        {isSearching && <button onClick={handleSearchClick} disabled={!searchText.length}>Cerrar</button>}
       </div>
     </div>
   );
---
+++ buscador-local/src/views/Search/components/SearchResults/SearchResultsItems.js
@@ -0,0 +1,16 @@
+export default function SearchresultsItem( { name, email }) {
+  return (
+    <div
+      style={{
+        backgroundColor: "#E8E7E7",
+        marginTop: "1rem",
+        marginBottom: "1rem",
+        padding: 10,
+        width: "100%",
+      }}
+    >
+      <p>{name}</p>
+      <p>{email}</p>
+    </div>
+  );
+}
