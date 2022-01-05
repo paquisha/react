8851c346e19bc1ed7890db99e4592dd84b174fe1
aekmot <paquishaxd@gmail.com>
2022-01-04 21:30:22 -0500

    se termina generador de memes

--- memes/src/App.js
+++ memes/src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import "./App.css";
+   
+import html2canvas from 'html2canvas';
 
 function App() {
+
+  const [lineaUno, setLineaUno] = useState('');
+
+  const [lineaDos, setLineaDos] = useState('');
+
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLineaUno = function(valor){
+    console.log(valor.target.value);
+    setLineaUno(valor.target.value);
+  }
+
+  const onChangeLineaDos = function(valor){
+    setLineaDos(valor.target.value);
+  }
+
+  const onChangeImage = function(valor){
+    setImagen(valor.target.value);
+  }
+
+  const onClickExportar = function (evento) {
+
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container pt-5">
+        <select onChange={onChangeImage} className="form-select">
+          <option value="homero">Homero</option>
+          <option value="kirby">kirby</option>
+          <option value="linux">Linux</option>
+          <option value="morocha">Morocha</option>
+          <option value="skinner">skiner</option>
+          <option value="malcon">malcom</option>
+          <option value="spiderMan">spider man</option>
+        </select>
+        <div className="row pt-5">
+          <input onChange={onChangeLineaUno} className="form-control" type="text" placeholder="Linea superior..."/>
+        </div>
+        <div className="row pt-5">
+          <input onChange={onChangeLineaDos} className="form-control" type="text" placeholder="Linea inferior..." />
+        </div>
+        <div className="row pt-5">
+          <button onClick={onClickExportar} type="button" className="btn btn-success" >Exportar</button>
+        </div>
+        <div className="row pt-5 meme" id="meme">
+          <span>{lineaUno}</span>
+          <br />
+          <span>{lineaDos}</span>
+          <img src={"/img/"+ imagen +".png"} />
+        </div>
+      </div>
     </div>
   );
 }
