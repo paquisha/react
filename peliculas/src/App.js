aab800ea7ecd895e07ad9f91625973bb24275fc6
aekmot <paquishaxd@gmail.com>
2022-01-05 23:43:48 -0500

    paginacion prueba

--- peliculas/src/App.js
+++ peliculas/src/App.js
@@ -1,24 +1,38 @@
-import logo from "./logo.svg";
+import { useState } from 'react';
 import "./App.css";
 import Pelicula from "./Components/Pelicula";
 import PageWrapper from "./Components/PageWrapper";
+import PeliculasJson from './peliculas.json';
+import Paginacion from "./Components/Paginacion";
 
 function App() {
+
+  const [paginaActual, setPaginaActual] = useState(1);
+
+  let peliculas = PeliculasJson;
+
+  peliculas = peliculas.slice((paginaActual - 1) * 3, paginaActual * 3);
+
   return (
     <PageWrapper>
-      <Pelicula
-        titulo="oblivion (2012)"
-        calificacion="8.1"
-        director="Joss Whedon"
-        actores="Robert Downey Jr, Chris Evans, Chris Hemsworth"
-        fecha="1 May 2015"
-        duracion="2h21"
-        img="images/uploads/mv1.jpg"
-      >
-        Earth's mightiest heroes must come together and learn to fight as a team
-        if they are to stop the mischievous Loki and his alien army from
-        enslaving humanity...
-      </Pelicula>
+      {peliculas.map((pelicula) => {
+        return (
+          <Pelicula
+            titulo={pelicula.titulo}
+            calificacion={pelicula.calificacion}
+            director={pelicula.director}
+            actores={pelicula.actores}
+            fecha={pelicula.fecha}
+            duracion={pelicula.duracion}
+            img={pelicula.img}
+          >
+            {pelicula.descripcion}
+          </Pelicula>
+        );
+      })}
+      <Paginacion pagina={paginaActual} total={4} onChange={(pagina) => {
+        setPaginaActual(pagina)
+      }} />
     </PageWrapper>
   );
 }
