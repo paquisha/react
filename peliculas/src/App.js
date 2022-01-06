ab2e3e41515c94d68a2d95bf0901ce049e1b687f
aekmot <paquishaxd@gmail.com>
2022-01-06 00:33:02 -0500

    tratar de cargar de una url

--- peliculas/src/App.js
+++ peliculas/src/App.js
@@ -1,38 +1,63 @@
-import { useState } from 'react';
-import "./App.css";
-import Pelicula from "./Components/Pelicula";
-import PageWrapper from "./Components/PageWrapper";
-import PeliculasJson from './peliculas.json';
-import Paginacion from "./Components/Paginacion";
+import { useState, useEffect } from 'react';
+import './App.css';
+import PageWrapper from './Components/PageWrapper';
+import Paginacion from './Components/Paginacion';
+import Pelicula from './Components/Pelicula';
+import peliculasJson from './peliculas.json';
 
 function App() {
 
   const [paginaActual, setPaginaActual] = useState(1);
+  const [peliculas, setPeliculs] = useState([]);
 
-  let peliculas = PeliculasJson;
+  useEffect(() => {
+    buscarPeliculas();
+  }, [])
 
-  peliculas = peliculas.slice((paginaActual - 1) * 3, paginaActual * 3);
+  const TOTAL_POR_PAGINA = 7;
+
+  let peliculs = peliculasJson;
+
+  const buscarPeliculas = async () => {
+    let url = 'https://lucasmoy.dev/data/react/peliculas.json';
+
+    let resultado = await fetch(url, {
+      "method": "GET",
+      "mode": 'no-cors',
+      "headers": {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json'
+      }
+    });
+    let json = await resultado.json();
+    setPeliculs(json);
+    console.log(json);
+  }
+
+  const getTotalPaginas = () => {
+    let cantidadTotalDePeliculas = peliculasJson.length;
+    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
+  }
+
+  let peliculasPorPagina = peliculas.slice(
+    (paginaActual - 1) * TOTAL_POR_PAGINA,
+    paginaActual * TOTAL_POR_PAGINA
+  );
 
   return (
     <PageWrapper>
-      {peliculas.map((pelicula) => {
-        return (
-          <Pelicula
-            titulo={pelicula.titulo}
-            calificacion={pelicula.calificacion}
-            director={pelicula.director}
-            actores={pelicula.actores}
-            fecha={pelicula.fecha}
-            duracion={pelicula.duracion}
-            img={pelicula.img}
-          >
-            {pelicula.descripcion}
-          </Pelicula>
-        );
-      })}
-      <Paginacion pagina={paginaActual} total={4} onChange={(pagina) => {
+      {peliculasPorPagina.map(pelicula =>
+        <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion}
+          director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha} duracion={pelicula.duracion}
+          img={pelicula.img}>
+          {pelicula.descripcion}
+        </Pelicula>
+      )}
+
+      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
         setPaginaActual(pagina)
       }} />
+
     </PageWrapper>
   );
 }
--- peliculas/src/Components/Paginacion.js
+++ peliculas/src/Components/Paginacion.js
@@ -1,33 +1,31 @@
 export default function Paginacion(props) {
-  //este metodo es mas facil de leer que el de la paginacion de abajo llamarlo {getpaginas}
-  const getPaginas = () => {
-    const resultado = [];
-    for (let i = 0; i < props.total; i++) {
-        let pagina = i + 1;
-      resultado.push(
-        <a
-          onclick={() => props.onChange(pagina)}
-          className={props.pagina === pagina ? "active" : ""}
-        >
-          {pagina}
-        </a>
-      );
-    }
-  };
-  return (
-    <div className="topbar-filter">
-      <label>Movies per page:</label>
-      <select>
-        <option value="range">5 Movies</option>
-        <option value="saab">10 Movies</option>
-      </select>
-      <div className="pagination2">
-        <span>
-          Pagina {props.pagina} de {props.total}:
-        </span>
 
-        {getPaginas()}
+
+
+    const getPaginas = () => {
+      const resultado = [];
+      for (let i = 0; i < props.total; i++) {
+        let pagina = i + 1;
+        resultado.push(
+          <a onClick={() => props.onChange(pagina)}
+            className={props.pagina === pagina ? 'active' : ''}>
+            {pagina}
+          </a>
+        );
+      }
+      return resultado;
+    }
+  
+  
+    return (
+      <div className="topbar-filter">
+  
+        <div className="pagination2">
+          <span>Página {props.pagina} de {props.total}:</span>
+  
+          {getPaginas()}
+  
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
