bc92cd15a6d5d9ad132226a165297f0bf57f68bb
paquisha <paquishaxd@gmail.com>
2022-05-13 15:21:03 -0500

    se agregan los ultimos cambios del ejercicio con props

--- testimonios/src/components/Testimonio.jsx
+++ testimonios/src/components/Testimonio.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import '../hojas-de-estilos/Testimonios.css';
 
-export default function Testimonio() {
+export default function Testimonio(props) {
   return (
     <div className='contenerdor-testimonio'>
       <img className='imagen-testimonio' 
-        src={require('../images/aime.jpg')}
+        src={require(`../images/${props.imagen}.jpg`)}
         alt='foto de Aime' />
         <div className='contenedor-texto-testimonio'>
-          <p className='nombre-testimonio'>Aime Pachacama en Ecuador</p>
-          <p className='cargo-testimonio'>Ingeniera Ambiental en Ecuador</p>
-          <p className='texto-testimonio'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, atque magnam? Expedita nam architecto totam placeat cupiditate ab ratione ut natus animi magni dolorem, saepe esse, voluptates doloremque culpa quos.</p>
+          <p className='nombre-testimonio'>
+            <b>{props.nombre}</b> en {props.pais}
+            </p>
+          <p className='cargo-testimonio'>{props.cargo} en <b>{props.empresa}</b></p>
+          <p className='texto-testimonio'>{props.testimonio}</p>
         </div>
     </div>
   )
